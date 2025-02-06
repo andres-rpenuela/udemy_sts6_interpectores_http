473288c217a7deb71e2ae5210deee9dc0954d905
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2025-02-06 14:23:48 +0100

    [sts6] create project empty for register time acces client with http interceptors

---
+++ spring-boot-3-register-time-access-client-interceptor/src/main/java/com/tokioschool/spring/SpringBoot3RegisterTimeAccessClientInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.tokioschool.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3RegisterTimeAccessClientInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3RegisterTimeAccessClientInterceptorApplication.class, args);
+	}
+
+}
