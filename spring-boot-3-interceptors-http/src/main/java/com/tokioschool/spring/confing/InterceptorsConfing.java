4bf0c9c74b1db071388fd84062f0fcadf6c61579
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2025-02-06 10:22:52 +0100

    [sts6] apply or excluded path patterns the http interceptors

--- spring-boot-3-interceptors-http/src/main/java/com/tokioschool/spring/confing/InterceptorsConfing.java
+++ spring-boot-3-interceptors-http/src/main/java/com/tokioschool/spring/confing/InterceptorsConfing.java
@@ -18,8 +18,14 @@ public class InterceptorsConfing implements WebMvcConfigurer  {
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loggerInterceptor);
-		registry.addInterceptor(timeInterceptor);
+		// aplica por defecto a todas, o indicar que solo aplique a
+		//registry.addInterceptor(loggerInterceptor).addPathPatterns("/**");// lo que aplica por defecto, poner esto es opcional
+		//registry.addInterceptor(loggerInterceptor).addPathPatterns("/api/some/**");// lo que aplica a todo lo que cuelque de /api/some
+		//registry.addInterceptor(loggerInterceptor).addPathPatterns("/api/some/bar");// aplica solo a este handler
+		registry.addInterceptor(loggerInterceptor).addPathPatterns("/api/some/bar","/api/some/foo");// aplica solo a estos dos hanler solo
+		
+		// a todas, menos a
+		registry.addInterceptor(timeInterceptor).excludePathPatterns("/api/some/bar");
 	}
 
 }
